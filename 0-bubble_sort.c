c4816ecf612f9e6be02905ad793bd66c648f1b1a
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-08-15 18:55:32 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,27 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * bubble_sort - a function that sorts an array of integers in ascg order
- * @array: sort array.
- * @size: size of array.
- * Return: Void function.
- */
-
+  * bubble_sort - sorts an array of ints in ascending order.
+  * @array: array to be sorted
+  * @size: size of the array
+  *
+  */
 void bubble_sort(int *array, size_t size)
 {
-size_t tmp, traverse, j;
+	int temp;
+	unsigned int i, j;
 
-if (array == NULL || size < 2)
-{
-return;
-}
-
-/** loop then traverseerse an array */
-for (traverse = 1; traverse < size; traverse++)
-{
-/** loop index the element */
-for (j = 0; j < size - 1; j++)
-{
-/** freater element */
-if (array[j] > array[j + 1])
-{
-/** the swap of elements */
-tmp = array[j];
-array[j] = array[j + 1];
-array[j + 1] = tmp;
-print_array(array, size);
-}
-}
-}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array((const int *)array, size);
+			}
+		}
+	}
 }
